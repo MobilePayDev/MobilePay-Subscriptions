0bbd8d827485eadc02f6d48f50261ba04a7dee04
Sophia Bergmann <sey@danskebank.dk>
2020-03-02 13:46:48 +0100

    Create ConsoleHelpers.cs

---
+++ docs/ClientExamples/helpers/ConsoleHelpers.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Runtime.InteropServices;
+using IdentityModel.OidcClient;
+
+namespace ConnectClientExample.Helpers
+{
+    public static class ConsoleHelpers
+    {
+        // Hack to bring the Console window to front.
+        // ref: http://stackoverflow.com/a/12066376
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        public static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        public static void BringConsoleToFront()
+        {
+            SetForegroundWindow(GetConsoleWindow());
+        }
+        
+        public static void PrintLoginInformation(LoginResult result)
+        {
+            if (result.IsError)
+            {
+                Console.WriteLine("\n\nError:\n{0}", result.Error);    
+            }
+            else
+            {
+                Console.WriteLine("\n\nClaims:");
+                foreach (var claim in result.User.Claims)
+                {
+                    Console.WriteLine("{0}: {1}", claim.Type, claim.Value);
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Access token:\n{0}", result.AccessToken);
+
+                if (!string.IsNullOrWhiteSpace(result.RefreshToken))
+                {
+                    Console.WriteLine("Refresh token:\n{0}", result.RefreshToken);
+                }
+            }
+        }
+    }
+}
+
